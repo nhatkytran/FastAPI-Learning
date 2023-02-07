5b896eabf2b247e5f5d4f1e8a3b0394406a2f072
nhatkytran <nhatky.tran.2002@gmail.com>
2023-02-07 21:59:48 +0700

    FastAPI - 2

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get('/')
-async def root():
+def root():
     return {'message': 'Hell beautiful World'}
+
+
+@app.post('/createposts')
+def create_posts(payload=Body()):
+    print(payload)
+    return {'message': 'Create posts'}
