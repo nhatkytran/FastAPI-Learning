d93a944b9b5bc63646acdc4da5bcd30d14cf55d3
nhatkytran <nhatky.tran.2002@gmail.com>
2023-02-18 21:47:21 +0700

    FastAPI - 4

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, Extra, validator
+from enum import Enum
+from typing import List, Tuple
 
 
-class User(BaseModel):
-    age: int | None
-    name = 'Frlix D. Tran'
+def snake_to_camel_case(value: str) -> str:
+    if not isinstance(value, str):
+        raise ValueError('Value must be a string!')
+    words = value.split('_')
+    value = ''.join(word.title() for word in words if word)
+    return f'{value[0].lower()}{value[1:]}'
 
 
-try:
-    user = User(age=100)
-    print(user)
-except (ValidationError, TypeError) as error:
-    print('Something went wrong!')
-    if isinstance(error, TypeError):
-        print(error)
-    else:
-        print(error.json())
+class CustomBaseModel(BaseModel):
+    class Config:
+        alias_generator = snake_to_camel_case
+        extra = Extra.forbid
+        allow_population_by_field_name = True
 
 
-# assert user.id == 123
-# assert user_x.id == 123
-# assert isinstance(user_x.id, int)
+class PolygonType(Enum):
+    triangle = 3
+
+
+class PolygonModel(CustomBaseModel):
+    polygon_type: PolygonType
+    vertices: List[Tuple[int | float, int | float]]
+
+    @validator('vertices')
+    def validate_vertices(cls, value, values):
+        polygon_type = values.get('polygon_type')
+        if polygon_type:
+            num_vertices_required = polygon_type.value
+            if len(value) != num_vertices_required:
+                raise ValueError
+        return value
+
+
+print(PolygonModel(polygon_type=3,
+      vertices=[(1, 1), (1, 1), (1, 1)]))
+
+
+t = PolygonType(3)
+print(t)
